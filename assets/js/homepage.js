e39130ac3519658113b75ee19863c688bc0f15fd
Yazid Putra Muhammad Riziq <yazidputra95@gmail.com>
2025-03-21 09:14:45 +0700

    Finalisasi Homepage

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -22,6 +22,34 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 // Fungsi Tombol Sidebar
-document.getElementById("menuBtn").addEventListener("click", function() {
-    document.querySelector(".sidebar").classList.toggle("show")
-})
+document.getElementById("menuBtn").addEventListener("click", function(event) {
+    event.stopPropagation();
+    document.querySelector(".sidebar").classList.toggle("show");
+});
+
+document.getElementById("menuBtn").addEventListener("click", function(event) {
+    event.stopPropagation();
+    document.querySelector(".content", ".content-a-1", ".content-a-2", ".content-a-3").classList.toggle("swipe");
+});
+
+document.addEventListener("click", function(event) {
+    const sidebar = document.querySelector(".sidebar");
+    const content = document.querySelector(".content");
+    const content1 = document.querySelector(".content-a-1");
+    const content2 = document.querySelector(".content-a-2");
+    const content3 = document.querySelector(".content-a-3");
+    const menuBtn = document.getElementById("menuBtn");
+
+    if (
+        !sidebar.contains(event.target) && event.target !== menuBtn &&
+        !content1.contains(event.target) && !content2.contains(event.target) &&
+        !content3.contains(event.target) && event.target !== menuBtn
+    ) {
+        sidebar.classList.remove("show");
+        content.classList.remove("swipe");
+        content1.classList.remove("swipe");
+        content2.classList.remove("swipe");
+        content3.classList.remove("swipe");
+    }
+});
+
